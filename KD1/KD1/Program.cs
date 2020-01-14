d700eb35c9dd0abfbc8feed64caaa4996123f2d9
tofanijo <tofanijo@gmail.com>
2020-01-14 20:17:55 +0200

    KD1

---
+++ KD1/KD1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KD1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string vardas = "Romanas";
+            isvestiVarda(vardas);
+            Console.ReadLine();
+        }
+
+        static void isvestiVarda(string vardas)
+        {
+            Console.WriteLine(vardas);
+        }
+
+        
+    }
+}

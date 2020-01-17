8339bdb14fb2a489de6def91d1c19635810f4de4
tofanijo <tofanijo@gmail.com>
2020-01-17 12:49:06 +0200

    KD7

---
+++ KD7/KD7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KD7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Parašyti funkciją, kuri pakelią vieną skaičių į antrojo laipsnį
+            //Pvz.: 2 ^ 3 = 8
+
+            double skaicius = 2;
+            int laipsnis = 3;
+
+            Console.WriteLine($"Skaiciaus {skaicius} pakeltas {laipsnis}-iuoju laipsniu lygus {pakeltiSkaiciuLaipsniu(skaicius, laipsnis)}");
+
+            Console.ReadLine();
+        }
+        static double pakeltiSkaiciuLaipsniu(double skaicius, int laipsnis)
+        {
+            double pakeltasSkaiciusLaipsniu = Math.Pow(skaicius, laipsnis);
+            return pakeltasSkaiciusLaipsniu;
+        }
+    }
+}

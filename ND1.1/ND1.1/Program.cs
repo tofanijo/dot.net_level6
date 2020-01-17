7a17b1a0a414982b92880d397f07a8ae69bbb330
tofanijo <tofanijo@gmail.com>
2020-01-17 18:31:00 +0200

    ND1._1(Cezario sifras)

---
+++ ND1.1/ND1.1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ND1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.Parašyti metodą, kuris gražina užšifruotą eilutę.
+            //a.Idėja tokia: paimti pirma eilutės simbolį, paversti jį į byte, pridėti bet kokį skaičių.Pradžioje išbandykite su 1.rezultatą paversti atgal į char.Šitaip padaryti su visais simboliais
+            //b.Rezultate raidė „a“ taps „b“, „b“ -> „c“ ir tt.
+            //c.Tai yra taip vadinamas Cezario Šifras(arba postūmio šifas). 
+            char c = 'a';
+            int i = (int)c;
+            Console.WriteLine("c:{0}, (int)c:{ 1}", c, i);
+            Console.ReadLine();
+
+            i++;
+                c = (char)i;
+            Console.WriteLine("c:{0}, (int)c:{ 1}", c, i);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
